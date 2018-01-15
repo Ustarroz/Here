3576d488f4cc1ae2ec5b683a7ecd7ff0a9b21a8b
Ustarroz <robin.ustarroz@epitech.eu>
2018-01-15 17:15:47 +0100

    added principals variables, init viewdidload and init ar scene

--- Here/New Group/MainVC.swift
+++ Here/New Group/MainVC.swift
@@ -7,22 +7,67 @@
 //
 
 import UIKit
+import CoreLocation
+import PusherSwift
+import Firebase
+import ARKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, ARSCNViewDelegate, CLLocationManagerDelegate {
+    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var statusTextView: UITextView!
+    
+    let locationManager = CLLocationManager()
+    var UserLocation = CLLocation()
+    var modelNode:SCNNode!
+    let rootNodeName = "exclamation"
+    var originalTransform:SCNMatrix4!
+    var heading : Double! = 0.0
+    var distance : Float = 0.0 {
+        didSet {
+            setStatusText()
+        }
+    }
+    var status: String! {
+        didSet {
+            setStatusText()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.worldAlignment = .gravityAndHeading
+        sceneView.session.run(configuration)
 
-    @IBOutlet weak var statusTextView: ARSCNView!
-    @IBOutlet weak var sceneView: UITextView!
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        sceneView.delegate = self
+        let scene = SCNScene()
+        sceneView.scene = scene
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        status = "Getting user location"
+        statusTextView.textContainerInset = UIEdgeInsetsMake(20.0, 10.0, 10.0, 0.0)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func setStatusText (){
+        var text = "Status: \(status!)\n"
+        text += "Distance: \(String(format: "%.2f m", distance))"
+        statusTextView.text = text
+    }
 
 }
 
