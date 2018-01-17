f76e80eb5fe3b2ef1e63907e12ee4247f0d2d1e3
Ustarroz <robin.ustarroz@epitech.eu>
2018-01-17 17:49:23 +0100

    FindVC design + gesture keyboard now hide when return button is pressed

--- Here/New Group/FindVC.swift
+++ Here/New Group/FindVC.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class FindVC: UIViewController {
+class FindVC: UIViewController, UITextFieldDelegate {
 
+   
+    @IBOutlet weak var IdUserTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        IdUserTextField.delegate = self
 
         // Do any additional setup after loading the view.
     }
@@ -22,14 +26,8 @@ class FindVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
     }
-    */
-
 }
