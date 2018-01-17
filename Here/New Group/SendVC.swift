673dc1d361ee97a571cdc7cee5263ab1cfa24e75
Ustarroz <robin.ustarroz@epitech.eu>
2018-01-17 17:51:09 +0100

    keyboard now hide when pressing return button on SendVC

--- Here/New Group/SendVC.swift
+++ Here/New Group/SendVC.swift
@@ -8,28 +8,23 @@
 
 import UIKit
 
-class SendVC: UIViewController {
+class SendVC: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var UserIdTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        UserIdTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
     }
-    */
 
 }
