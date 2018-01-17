28812a3621d7ccbd9d67366d3f51b0f02d4acfe2
Ustarroz <robin.ustarroz@epitech.eu>
2018-01-17 15:17:12 +0100

    Add menu page + send and find storyboard

---
+++ Here/New Group/ChoiceVC.swift
@@ -0,0 +1,33 @@
+//
+//  ChoiceVC.swift
+//  Here
+//
+//  Created by robin ustarroz on 17/01/2018.
+//  Copyright © 2018 robin ustarroz. All rights reserved.
+//
+
+import UIKit
+
+class ChoiceVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func sendPositionIsPushed(_ sender: Any) {
+        
+    }
+    
+ 
+    @IBAction func findFriendsIsPushed(_ sender: Any) {
+        
+    }
+ 
+}
